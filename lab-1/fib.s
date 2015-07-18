900ca6ae44d860136d757a75d07a3795937b9423
王信凱 <jcskyting@gmail.com>
2015-07-18 21:04:25 +0800

    bl fibonacci

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -16,7 +16,7 @@ fibonacci:
 	@ R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
 	sub r4, r0, #0
-	ble r0, #0
+	beq .L3
 
 	@ Compare R4 with 1
 	@ If R4 == 1 goto .L4 (which returns 1)
@@ -26,17 +26,17 @@ fibonacci:
 	@ R0 = R4 - 1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
 	sub r0, r4, #1
-
+	bl fibonacci
 
 	@ R5 = R0
 	@ R0 = R4 - 2
 	@ Recursive call to fibonacci with R4 - 2 as parameter
 	mov r5, r0
 	sub r0, r4, #2
+	bl fibonacci
 
 	@ R0 = R5 + R0 (update flags)
 	add r0, r5, r0
-
 	pop {r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -26,16 +26,17 @@ fibonacci:
 	@ R0 = R4 - 1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
 	sub r0, r4, #1
+	bl fibonacci
 
 	@ R5 = R0
 	@ R0 = R4 - 2
 	@ Recursive call to fibonacci with R4 - 2 as parameter
 	mov r5, r0
 	sub r0, r4, #2
+	bl fibonacci
 
 	@ R0 = R5 + R0 (update flags)
 	add r0, r5, r0
-
 	pop {r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
